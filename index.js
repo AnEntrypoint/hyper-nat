2111af1fe793851fffc0a2f28d78ce05245ece80
lanmower <almagestfraternite@gmail.com>
2024-04-29 19:57:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -63,8 +63,11 @@ const relay = async () => {
                     socket.on('error', localend)
                     socket.on('end', localend)
                 });
-                server.listen(port, "127.0.0.1");
-                console.log('listening for local connections on tcp', port);
+                const socket = node.connect(publicKey, { reusableSocket: true })
+                socket.on('ready', (d)=>{socket.end()})
+                
+                server.listen(port, "127.0.0.1")
+                console.log('listening for local connections on tcp', port)
             }
         },
         udp: {
